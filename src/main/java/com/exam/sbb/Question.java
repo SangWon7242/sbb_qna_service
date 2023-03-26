c0f340f35242d1ad01d91c5213a9935c37c3aec4
SangWon7242 <gotkdqja@gmail.com>
2023-03-26 10:40:46 +0900

    question truncate 테스트 케이스 추가

--- src/main/java/com/exam/sbb/Question.java
+++ src/main/java/com/exam/sbb/Question.java
@@ -1,9 +1,9 @@
 package com.exam.sbb;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
