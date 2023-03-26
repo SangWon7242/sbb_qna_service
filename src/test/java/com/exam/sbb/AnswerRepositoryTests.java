a1d6b8eaf9abce312a98ad1a8496eb79a049ca4b
SangWon7242 <gotkdqja@gmail.com>
2023-03-26 11:44:46 +0900

    테스트 데이터 저장시 마다 쌓이지 않게

--- src/test/java/com/exam/sbb/AnswerRepositoryTests.java
+++ src/test/java/com/exam/sbb/AnswerRepositoryTests.java
@@ -1,5 +1,6 @@
 package com.exam.sbb;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,6 +14,23 @@ public class AnswerRepositoryTests {
 
   @Autowired
   private AnswerRepository answerRepository;
+  private int lastSampleDataId;
+
+  @BeforeEach
+  void beforeEach() {
+    clearData();
+    createSampleData();
+  }
+
+  private void createSampleData() {
+
+  }
+
+  private void clearData() {
+    questionRepository.disableForeignKeyCheck();
+    answerRepository.truncate();
+    questionRepository.enableForeignKeyCheck();
+  }
 
   @Test
   void  저장() {
