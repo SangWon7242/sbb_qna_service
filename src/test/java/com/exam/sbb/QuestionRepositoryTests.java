16ffea2400c98402b9ab02bed21c781a37e9d1d2
SangWon7242 <gotkdqja@gmail.com>
2023-04-02 09:29:07 +0900

    양방향 관계 만들어주기

--- src/test/java/com/exam/sbb/QuestionRepositoryTests.java
+++ src/test/java/com/exam/sbb/QuestionRepositoryTests.java
@@ -29,6 +29,7 @@ public class QuestionRepositoryTests {
     q1.setCreateDate(LocalDateTime.now());
     questionRepository.save(q1);
 
+
     Question q2 = new Question();
     q2.setSubject("스프링부트 모델 질문입니다.");
     q2.setContent("id는 자동으로 생성되나요?");
