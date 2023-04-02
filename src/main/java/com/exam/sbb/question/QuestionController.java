a722a5140a097f2bad3d63297bf62ff1c7d811e4
SangWon7242 <gotkdqja@gmail.com>
2023-04-02 10:09:01 +0900

    question_list.html 뷰 파일 추가

--- src/main/java/com/exam/sbb/question/QuestionController.java
+++ src/main/java/com/exam/sbb/question/QuestionController.java
@@ -8,8 +8,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class QuestionController {
 
   @GetMapping("/question/list")
-  @ResponseBody
+  // 이 자리에 @ResponseBody가 없으면 resources/templates/question_list.html 파일을 뷰로 삼는다.
   public String list() {
-    return "question list";
+    return "question_list";
   }
+
 }
