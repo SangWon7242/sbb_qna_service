745c939898477a5b521b6c0dcd13c74ac208e13c
SangWon7242 <gotkdqja@gmail.com>
2023-04-16 09:42:28 +0900

    h2 콘솔에 대해서 스프링 시큐리티가 작동하지 않도록

--- src/main/java/com/exam/sbb/SecurityConfig.java
+++ src/main/java/com/exam/sbb/SecurityConfig.java
@@ -1,10 +1,12 @@
 package com.exam.sbb;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -13,10 +15,14 @@ public class SecurityConfig {
   @Bean
   SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests().requestMatchers(
-        // 모든 경로
-        new AntPathRequestMatcher("/**"))
-        // 허용 한다.
-        .permitAll();
+        new AntPathRequestMatcher("/**")).permitAll()
+        .and() // 문맥의 끝
+        .csrf().ignoringRequestMatchers(
+            new AntPathRequestMatcher("/h2-console/**"))
+        .and()
+        .headers()
+        .addHeaderWriter(new XFrameOptionsHeaderWriter(
+            XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN));
     return http.build();
   }
 }
