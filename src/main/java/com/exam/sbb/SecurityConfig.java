85d53d90b60472ab598cbae3bff4e835942a089b
SangWon7242 <gotkdqja@gmail.com>
2023-04-16 09:19:21 +0900

    시큐티 설정, 모든 경로에 대한 접속 허용

---
+++ src/main/java/com/exam/sbb/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.exam.sbb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+  @Bean
+  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests().requestMatchers(
+        // 모든 경로
+        new AntPathRequestMatcher("/**"))
+        // 허용 한다.
+        .permitAll();
+    return http.build();
+  }
+}
