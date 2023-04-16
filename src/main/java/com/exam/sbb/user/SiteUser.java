894f5c2b6ed0829a5d41409ca8b980c6465fe7ad
SangWon7242 <gotkdqja@gmail.com>
2023-04-16 11:22:22 +0900

    상단바에 회원가입링크 넣기

--- src/main/java/com/exam/sbb/user/SiteUser.java
+++ src/main/java/com/exam/sbb/user/SiteUser.java
@@ -3,6 +3,7 @@ package com.exam.sbb.user;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
@@ -20,4 +21,5 @@ public class SiteUser {
 
   @Column(unique = true)
   private String email;
+
 }
