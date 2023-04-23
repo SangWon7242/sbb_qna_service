f182824a48828bd885cec374a8be45809feb8b34
SangWon7242 <gotkdqja@gmail.com>
2023-04-23 11:51:00 +0900

    질문 삭제 처리

--- src/main/java/com/exam/sbb/question/QuestionController.java
+++ src/main/java/com/exam/sbb/question/QuestionController.java
@@ -70,7 +70,7 @@ public class QuestionController {
     Question question = this.questionService.getQuestion(id);
 
     if(question == null) {
-      throw new DataNotFoundException("%d번 질문은 존재하지 않습니다.".formatted(id));
+      throw new DataNotFoundException("%d번 질문은 존재하지 않습니다.");
     }
 
     if(!question.getAuthor().getUsername().equals(principal.getName())) {
@@ -95,6 +95,24 @@ public class QuestionController {
     }
     questionService.modify(question, questionForm.getSubject(), questionForm.getContent());
     return String.format("redirect:/question/detail/%s", id);
+  }
+
+  @PreAuthorize("isAuthenticated()")
+  @GetMapping("/delete/{id}")
+  public String questionDelete(Principal principal, @PathVariable("id") Integer id) {
+    Question question = questionService.getQuestion(id);
+
+    if(question == null) {
+      throw new DataNotFoundException("%d번 질문은 존재하지 않습니다.");
+    }
+
+    if (!question.getAuthor().getUsername().equals(principal.getName())) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+    }
+
+    questionService.delete(question);
+
+    return "redirect:/";
   }
 
   @PreAuthorize("isAuthenticated()")
