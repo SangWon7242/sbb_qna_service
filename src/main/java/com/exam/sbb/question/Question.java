79e0f0695447ba187465cd5df2b2efbfb9a614f6
SangWon7242 <gotkdqja@gmail.com>
2023-04-23 10:41:10 +0900

    Answer, Queston에 modifyDate 칼럼 추가

--- src/main/java/com/exam/sbb/question/Question.java
+++ src/main/java/com/exam/sbb/question/Question.java
@@ -5,6 +5,7 @@ import com.exam.sbb.user.SiteUser;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -25,7 +26,9 @@ public class Question {
   @Column(columnDefinition = "TEXT") // TEXT
   private String content;
 
+  @CreatedDate
   private LocalDateTime createDate;
+  private LocalDateTime modifyDate;
 
   @ManyToOne
   private SiteUser author;
