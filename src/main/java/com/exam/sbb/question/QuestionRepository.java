9c0853954df3b407fb283e40875d61dd18f98911
SangWon7242 <gotkdqja@gmail.com>
2023-04-30 11:51:53 +0900

    kw 파라미터가 질문제목 + 질문내용 + 질문작성자명 + 답변내용 검색, 중복 제거

--- src/main/java/com/exam/sbb/question/QuestionRepository.java
+++ src/main/java/com/exam/sbb/question/QuestionRepository.java
@@ -26,6 +26,8 @@ public interface QuestionRepository extends JpaRepository<Question, Long>, Repos
 
   Page<Question> findBySubjectContainsOrContentContainsOrAuthor_usernameContainsOrAnswerList_contentContains(String kw, String kw_, String kw__, String kw___, Pageable pageable);
 
+  Page<Question> findDistinctBySubjectContainsOrContentContainsOrAuthor_usernameContainsOrAnswerList_contentContains(String kw, String kw_, String kw__, String kw___, Pageable pageable);
+
   @Transactional
   @Modifying
   @Query(value = "ALTER TABLE question AUTO_INCREMENT = 1", nativeQuery = true)
